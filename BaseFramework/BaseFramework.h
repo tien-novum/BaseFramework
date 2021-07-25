e4340700f3d43a31bbf628a1039f2692c291b418
tien-novum <tien@novum.capital>
2021-07-25 11:06:54 +0700

    Initial Commit

---
+++ BaseFramework/BaseFramework.h
@@ -0,0 +1,18 @@
+//
+//  BaseFramework.h
+//  BaseFramework
+//
+//  Created by Lai Trung Tien on 25/07/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaseFramework.
+FOUNDATION_EXPORT double BaseFrameworkVersionNumber;
+
+//! Project version string for BaseFramework.
+FOUNDATION_EXPORT const unsigned char BaseFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseFramework/PublicHeader.h>
+
+
